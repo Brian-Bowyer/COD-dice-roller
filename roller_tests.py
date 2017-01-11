d404ebf5b05ba8980449c1c731ce7675a88508c1
Brian-Bowyer <bobsnake64@gmail.com>
2017-01-11 12:04:15 -0600

    Defined testing skeleton

---
+++ roller_tests.py
@@ -0,0 +1,14 @@
+import os, unittest, tempfile
+import roller
+
+class RollerTestCase(unittest.TestCase):
+
+	def setup(self):
+		pass
+
+	def teardown(self):
+		pass
+
+
+if __name__ == '__main__':
+	unittest.main()
