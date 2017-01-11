807e906ced34cf5bb9343ee149ac3ba0c575680f
Brian-Bowyer <bobsnake64@gmail.com>
2017-01-11 12:17:19 -0600

    Added empty form response
    Frontend now sends pool to backend code. Backend code still does not
    compute result.

--- roller.py
+++ roller.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from roller_processing import compute_successes
 app = Flask(__name__)
 
 @app.route('/')
 def roller():
 	return render_template("index.html")
+
+@app.route('/recieve_pool', methods=["POST"])
+def recieve_pool():
+	pool = request.form['pool']
+	return render_template("index.html", successes = compute_successes(pool))
---
+++ roller_processing.py
@@ -0,0 +1,4 @@
+import random
+
+def compute_successes(pool):
+	pass
